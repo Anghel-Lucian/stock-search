1ff0c83082854e33cf1ca36c3ac893501d0afc30
Anghel-Lucian <anghelpaul99@gmail.com>
2022-04-02 11:59:18 +0300

    initialized app with vite

---
+++ README.md
@@ -0,0 +1,6 @@
+Built on Node.js v14.0.0 and NPM v6.14.4
+
+This application was created using the Vite JavaScript bundler.
+
+Start with:
+```npm run dev```
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class App extends React.PureComponent {
+	render() {
+		return (
+			<p>Welcome to stock searcher</p>
+		);
+	}
+}
